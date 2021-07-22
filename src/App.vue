3817a5da416b5e7e835ff6a7eadaff43f79c6de2
Johnny Lay <johnny.lay.developer@gmail.com>
2021-07-22 14:26:45 +0200

    Renamed Timer component + css styling and added lifecycle hook for Timer component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <h1>Reaction Timer ⏲</h1>
+  <main>
+    <h1>Reaction Timer ⏲</h1>
+    <div class="button-wrapper">
+      <button @click="start" :disabled="isStarting">Start timer</button>
+      <button @click="instruction">Instructions</button>
+    </div>
+    <Play v-if="isStarting" :props="{ reactionDelay }" />
+  </main>
 </template>
 
 <script>
+import Play from "./components/game/Timer.vue";
+
 export default {
   name: "App",
-  components: {},
+
+  components: { Play },
+
+  data() {
+    return {
+      isStarting: false,
+      reactionDelay: null,
+    };
+  },
+
+  methods: {
+    start() {
+      // Sets the delay of game reaction between: 2-7 seconds.
+      this.reactionDelay = 2000 + Math.random() * 5000;
+
+      // Starts the game by setting "isStarting" to: true.
+      this.isStarting = true;
+
+      return;
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/game/Timer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="timer-block" v-if="showTimerBlock">Click me</div>
+</template>
+
+<script>
+export default {
+  name: "Timer",
+
+  // Lifecycle Hooks
+  mounted() {
+    // Show "game block" when reactionDelay is completed.
+    setTimeout(() => {
+      // set showTimerBlock to: true.
+      this.showTimerBlock = true;
+    }, this.props.reactionDelay);
+  },
+
+  data() {
+    return {
+      showTimerBlock: false,
+    };
+  },
+
+  methods: {},
+
+  props: {
+    props: Object,
+  },
+};
+</script>
+
+<style scoped>
+.timer-block {
+  background: #53b8bb;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  margin: 40px auto;
+  padding: 100px 200px;
+  text-align: center;
+}
+</style>
